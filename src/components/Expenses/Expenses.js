e338310a01ca50203c4493ad794226c3e11d6e6b
JagodaBodnar <jagodabodnar90@gmail.com>
2021-09-13 21:33:59 +0200

    Chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import ExpenseFilter from "./ExpensesFilter";
 import './Expenses.css'
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -14,6 +15,7 @@ const Expenses = (props) => {
     return (
         <div className="expenses">
             <ExpenseFilter selected={filteredYear} filterChangeHandler={filterChangeHandler}/>
+            <ExpensesChart expenses={filteredExpenses} />
             <ExpensesList filteredExpenses={filteredExpenses}  />
         </div>
     )
